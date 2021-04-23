628ce8bcb1be4acda5f3049316979c1a0bb8cc71
Gabriel Rufino <contato@gabrielrufino.com>
2021-04-22 22:43:56 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const camelCase = require('camelcase')
+const fs = require('fs')
+
+const filterModules = file => file.endsWith('.js')
+const mapModules = folder => module => (
+  [camelCase(module.replace('.js', '')), require(`${folder}/${module}`)]
+)
+
+/**
+ * Requires all the modules from a folder
+ * @param {String} folder
+ */
+module.exports = (folder, { async = false } = {}) => {
+  if (async) {
+    return fs
+      .promises
+      .readdir(folder)
+      .then(files => files.filter(filterModules))
+      .then(modules => modules.map(mapModules(folder)))
+      .then(Object.fromEntries)
+      .catch(error => error)
+  }
+
+  return Object.fromEntries(
+    fs
+      .readdirSync(folder)
+      .filter(filterModules)
+      .map(mapModules(folder))
+  )
+}
