00eb221877558d08d8684ca244232e6390707901
Gabriel Rufino <contato@gabrielrufino.com>
2025-05-29 18:25:30 -0300

    refactor: folquire

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ const mapModules = require('./helpers/map-modules');
 
 /**
  * Requires all the modules from a folder
- * @param {String} folder
- * @returns {Object}
+ * @param {String} folder - Path to the folder
+ * @param {Object} [options]
+ * @param {Boolean} [options.async=false] - Use async mode
+ * @param {Array} [options.ignore=[]] - List of files to ignore
+ * @returns {Object|Promise<Object>}
  */
 module.exports = (folder, { async = false, ignore = [] } = {}) => {
   if (typeof folder !== 'string') {
-    throw new TypeError('Folder must be an string');
+    throw new TypeError('Folder must be a string');
   }
 
   if (typeof async !== 'boolean') {
@@ -37,7 +40,7 @@ module.exports = (folder, { async = false, ignore = [] } = {}) => {
     fs
       .readdirSync(folder)
       .filter(filterModules)
-      .filter((filterIgnoredModules(ignore)))
+      .filter(filterIgnoredModules(ignore))
       .map(mapModules(folder)),
   );
 };
